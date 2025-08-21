4796932e9a7d609b837f7c0f60949753feebc9d8
Drew Day <drew.day04@gmail.com>
2025-08-21 16:44:48 -0400

    Initial Commit

---
+++ M1HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M1HelloWorld2
+//
+//  Created by Day, Drew S. on 8/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+import MapKit
+
+
+
+var greeting = "Hello, playground"
+print(greeting)
+
+UIImage(systemName: "circle")
+
+var sum = 0
+for i in 1...10 {
+    sum += i
+}
+
+MKMapView()
+
